26e81f28552a2bc0e4898b796672ef1be9d511e0
Gabriel Adam <gabrieladam79@gmail.com>
2018-05-10 12:50:01 -0300

    Aula1005_POO

---
+++ Aulas/Aula1005_POO/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aula1005_POO
+{
+    class Cliente : Pessoa
+    {
+
+        public DateTime DataNasc { get; set; }
+
+        public string DataQueNasceu
+        {
+            get
+            {
+                return DataNasc.ToShortDateString();
+            }
+        }
+
+        public int Idade
+        {
+            get
+            {
+                return DateTime.Today.Year - DataNasc.Year;
+            }
+        }
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(string nome) : base(nome)
+        {
+
+        }
+
+        public Cliente(string nome, string cpf) :
+            base(nome, cpf)
+        {
+
+        }
+
+
+    }
+}
---
+++ Aulas/Aula1005_POO/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿namespace Aula1005_POO
+{
+    public abstract class Pessoa
+    {
+        // Atributo
+        private string nome;
+
+        // Propriedades
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Cpf { get; set; }
+
+        // Construtores
+        public Pessoa() // Construtor vazio
+        {
+
+        }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
+    }
+}
